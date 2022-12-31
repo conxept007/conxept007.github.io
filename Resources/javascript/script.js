9cb84683d642e554903e8d27e9debaa690a50126
conxept007 <90760026+conxept007@users.noreply.github.com>
2022-12-31 21:46:22 +0100

    Trying to change navigation for mobile view with javascript

---
+++ Resources/javascript/script.js
@@ -0,0 +1,9 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+    } else {
+      x.style.display = "none";
+    }
+  }
